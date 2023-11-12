44cfa08602df543d1230430de3f76c097f5a2b5d
mukhtorov <mukhtorov_1994@mail.ru>
2023-11-12 11:55:57 +0500

    longsetCommon Runtime - 24% Memory-28%

---
+++ junior/4 longestCommonPrefix.js
@@ -0,0 +1,17 @@
+// link - https://leetcode.com/problems/longest-common-prefix/description/
+
+// Solution - 1
+// Runtime 61ms / 23.89%
+// Memory 42.86MB / 28.35%
+function solution1(strs = []) {
+  let res = "";
+  for (let i = 0; i < strs[0].length; i++) {
+    if (strs.every((str) => str[i] === strs[0][i])) res += strs[0][i];
+    else break;
+  }
+  return res;
+}
+
+// PS: lekin menga bu yoqmadi, keyin vaqt bo'lganda ko'rib chiqaman buni...
+
+console.log(solution1(["flower", "flower", "flightwer"]));
