3bc12e67bc14d09883e93ef2211a83d748b75a4c
mukhtorov <mukhtorov_1994@mail.ru>
2023-11-19 16:22:10 +0500

    #2703 / runtime 92% / Memory 75%

---
+++ junior/42 argumentsLength.js
@@ -0,0 +1,10 @@
+// No:2703 - https://leetcode.com/problems/return-length-of-arguments-passed/description/
+
+// Solution - 1
+// Runtime - 127ms 11.54%
+// Memory - 48.13MB 10%
+// P/S: Slow solution
+var solution1 = function (...args) {
+  return args.length;
+};
+// P/S: Very very easy
