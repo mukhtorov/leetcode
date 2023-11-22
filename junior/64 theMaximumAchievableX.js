6dfb821970e7337c68dd4cc6c47c6fa4a15866a0
mukhtorov <mukhtorov_1994@mail.ru>
2023-11-23 02:13:36 +0500

    #2769 / runtime 88% / Memory 96.5%

---
+++ junior/64 theMaximumAchievableX.js
@@ -0,0 +1,9 @@
+// No:2769 - https://leetcode.com/problems/find-the-maximum-achievable-number/description/
+
+// Solution - 1
+// Runtime - 76ms 88.27%
+// Memory - 44.94MB 96.53%
+
+var solution1 = function (num, t) {
+  return t * 2 + num;
+};
