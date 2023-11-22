b295861bf0e159f4f9b69f5c9582265135085e79
mukhtorov <mukhtorov_1994@mail.ru>
2023-11-23 01:58:32 +0500

    #1108 / runtime 89% / Memory 96.5%

---
+++ junior/63 defangIPaddr.js
@@ -0,0 +1,9 @@
+// No:1108 - https://leetcode.com/problems/defanging-an-ip-address/
+
+// Solution - 1
+// Runtime - 43ms 89.27%
+// Memory - 41.18MB 96.53%
+// PS: dehqoncha yo'li
+var solution1 = function (address) {
+  address.replaceAll(".", "[.]");
+};
