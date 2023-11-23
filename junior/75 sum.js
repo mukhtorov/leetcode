194167b9afb120a3a8d579c8e1c96dec3f2cbe0a
mukhtorov <mukhtorov_1994@mail.ru>
2023-11-23 17:00:18 +0500

    #2235 / runtime 100% / Memory 100%

---
+++ junior/75 sum.js
@@ -0,0 +1,12 @@
+// No:2235 - https://leetcode.com/problems/add-two-integers/
+
+// Solution - 1
+// Runtime - 49ms 82.65%
+// Memory - 42.14MB 72.95%
+// bulshit question ))
+
+var solution1 = function (num1, num2) {
+  return num1 + num2;
+};
+
+console.log(solution1(1, 3)); //
