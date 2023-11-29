10e9817fad8964fce8ba6813b5d7b036481b2717
mukhtorov <mukhtorov_1994@mail.ru>
2023-11-30 00:09:41 +0500

    #2778 runtime 94% / memory 85%

--- junior/106 sumOfSquares.js
+++ junior/106 sumOfSquares.js
@@ -17,16 +17,15 @@ var solution1 = function (nums) {
 };
 
 // Solution - 1
-// Runtime - 57ms 90.06%
+// Runtime - 54ms 94.06%
 // Memory - 43.28MB 85.58%
 
 var solution2 = function (nums) {
-  let length = nums.length;
   return nums.reduce((sum, val, i) => {
-    if (length % (i + 1) === 0) return sum + nums[i] ** 2;
+    if (nums.length % (i + 1) === 0) return sum + val ** 2;
     else return sum;
-  });
+  }, 0);
 };
 
-console.log(solution2([1, 2, 3, 4])); // 21
+// console.log(solution1([1, 2, 3, 4])); // 21
 console.log(solution2([2, 7, 1, 19, 18, 3])); // 63
